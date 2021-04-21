273fb8a34910122cc6a7038932e16bddf8edb02b
Professional <danila.stepanidenko@mail.ru>
2021-04-21 15:38:44 +0300

    version-0.1.0

---
+++ app/src/main/java/com/example/newmeetapp/LoginActivity.kt
@@ -0,0 +1,81 @@
+package com.example.newmeetapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.util.Patterns
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+        loginIn()
+    }
+
+    private fun loginIn()
+    {
+        loginButton.setOnClickListener {
+            if (validation()) {
+                auth.signInWithEmailAndPassword(emailLoginInput.text.toString(), passwordLoginInput.text.toString())
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            startActivity(Intent(this@LoginActivity, ProfileActivity::class.java))
+                            finish()
+                        } else {
+                            Log.w("createUserWithEmail:failure", it.exception)
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "Email or Password didn't match",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
+            }
+        }
+    }
+
+    private fun validation() : Boolean
+    {
+        if (TextUtils.isEmpty(emailLoginInput.text.toString()))
+        {
+            emailLoginInput.error = "Please enter email"
+            return false
+        }
+        else if (TextUtils.isEmpty(passwordLoginInput.text.toString()))
+        {
+            passwordLoginInput.error = "Please password"
+            return false
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailLoginInput.text.toString()).matches()) {
+            emailLoginInput.error = "Please Enter Valid Email"
+            return false
+        }
+        if (emailLoginInput.text.toString().split('@')[1] != "gmail.com")
+        {
+            emailLoginInput.error = "Please Enter corporation Email"
+            return false
+        }
+        return true
+    }
+
+    fun goToRegisterPage(view: View)
+    {
+        val pageRegister = Intent(this, RegistrationActivity::class.java)
+        startActivity(pageRegister)
+    }
+}
---
+++ app/src/main/java/com/example/newmeetapp/RegistrationActivity.kt
@@ -0,0 +1,112 @@
+package com.example.newmeetapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.util.Patterns
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_registration.*
+import org.w3c.dom.Text
+
+
+class RegistrationActivity : AppCompatActivity(){
+    lateinit var auth: FirebaseAuth
+    val MIN_PASSWORD_LENGTH = 6
+    private lateinit var databaseReference: DatabaseReference
+    var database: FirebaseDatabase? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        databaseReference = database?.reference!!.child("profile")
+
+        register()
+    }
+
+
+    private fun register()
+    {
+        registerButton.setOnClickListener {
+
+            if (validation()) {
+                Log.d("Переменные в базу - name", firstnameInput.text.toString())
+                Log.d("Переменные в базу - lastname", lastnameInput.text.toString())
+                auth.createUserWithEmailAndPassword(emailInput.text.toString(), passwordInput.text.toString()).addOnCompleteListener {
+                            if (it.isSuccessful) {
+
+                                val currentUser = auth.currentUser
+                                val currentUserDb = databaseReference.child(currentUser?.uid!!)
+                                currentUserDb.child("firstname").setValue(firstnameInput.text.toString())
+                                currentUserDb.child("lastname").setValue(lastnameInput.text.toString())
+                                Toast.makeText(this, "Registartion success!", Toast.LENGTH_SHORT).show()
+                                startActivity(Intent(this@RegistrationActivity, LoginActivity::class.java))
+                                finish()
+                            } else {
+                                Log.w("createUserWithEmail:failure", it.exception)
+                                Toast.makeText(this@RegistrationActivity, "Registartion failed, please try again", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+            }
+        }
+    }
+
+
+    private fun validation() : Boolean
+    {
+
+        if (TextUtils.isEmpty(firstnameInput.text.toString()))
+        {
+            firstnameInput.error = "Please enter first name"
+            return false
+        }
+
+        else if (TextUtils.isEmpty(lastnameInput.text.toString()))
+        {
+            lastnameInput.error = "Please enter last name"
+            return false
+        }
+        else if (TextUtils.isEmpty(emailInput.text.toString()))
+        {
+            emailInput.error = "Please enter email"
+            return false
+        }
+        else if (TextUtils.isEmpty(passwordInput.text.toString()))
+        {
+            passwordInput.error = "Please password"
+            return false
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailInput.text.toString()).matches()) {
+            emailInput.error = "Please Enter Valid Email"
+            return false
+        }
+        if (emailInput.text.toString().split('@')[1] != "gmail.com")
+        {
+            emailInput.error = "Please Enter corporation Email"
+            return false
+        }
+        if (passwordInput.text.toString().length < MIN_PASSWORD_LENGTH) {
+            passwordInput.error = "Password Length must be more than " + MIN_PASSWORD_LENGTH + "characters"
+            return false
+        }
+        if (passwordInput.text.toString() != repeatpasswordInput.text.toString())
+        {
+            repeatpasswordInput.error = "Password does not match"
+            return false
+        }
+        return true
+
+    }
+}
