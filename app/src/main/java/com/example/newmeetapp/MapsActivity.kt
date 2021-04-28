62a5c85e11efed7dd7eb5c2a8e228c96932c713a
Professional <danila.stepanidenko@mail.ru>
2021-04-28 13:01:25 +0300

    mapsACtivity

--- app/src/main/java/com/example/newmeetapp/MapsActivity.kt
+++ app/src/main/java/com/example/newmeetapp/MapsActivity.kt
@@ -13,6 +13,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.libraries.places.api.Places
@@ -21,14 +22,16 @@ import com.google.android.libraries.places.widget.Autocomplete
 import com.google.android.libraries.places.widget.AutocompleteActivity
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_registration.*
+import kotlin.reflect.typeOf
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var databaseReference: DatabaseReference
+    private lateinit var etPlace: Place
     var database: FirebaseDatabase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,15 +61,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         autocompleteFragment.setCountries("RU")
         // Specify the types of place data to return.
-        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG, Place.Field.ADDRESS))
+
 
         // Set up a PlaceSelectionListener to handle the response.
         autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
             override fun onPlaceSelected(place: Place) {
                 savePlaceToDB(place)
-//                addMarker(place)
                 // TODO: Get info about the selected place.
-                Log.i("autocomplete", "Place: ${place.name}, ${place.id}")
+                Log.i("autocomplete", "Place: ${place.name}, ${place.id},  ${place.latLng}, ${place.address}")
             }
 
             override fun onError(status: Status) {
@@ -83,8 +86,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val currentUserDb = databaseReference.child(place.id.toString())
         currentUserDb.child("name").setValue(place.name)
         currentUserDb.child("id").setValue(place.id)
-        currentUserDb.child("LatLng").setValue(place.latLng.toString())
-        currentUserDb.child("adress").setValue(place.address.toString())
+        currentUserDb.child("LatLng").setValue(place.latLng)
+        currentUserDb.child("address").setValue(place.address.toString())
         Toast.makeText(this, "The place will be added to DataBase", Toast.LENGTH_SHORT).show()
     }
 
@@ -99,31 +102,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(place.latLng))
 //    }
 
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
-//            when (resultCode) {
-//                Activity.RESULT_OK -> {
-//                    data?.let {
-//                        val place = Autocomplete.getPlaceFromIntent(data)
-//                        Log.i(this.toString(), "Place: ${place.name}, ${place.id}")
-//                    }
-//                }
-//                AutocompleteActivity.RESULT_ERROR -> {
-//                    // TODO: Handle the error.
-//                    data?.let {
-//                        val status = Autocomplete.getStatusFromIntent(data)
-//                        status.statusMessage?.let { it1 -> Log.i(this.toString(), it1) }
-//                    }
-//                }
-//                Activity.RESULT_CANCELED -> {
-//                    // The user canceled the operation.
-//                }
-//            }
-//            return
-//        }
-//        super.onActivityResult(requestCode, resultCode, data)
-//    }
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -133,15 +111,47 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    override fun onMapReady(googleMap: GoogleMap) {
+    override fun onMapReady(googleMap: GoogleMap)
+    {
+        //val user = auth.currentUser
         mMap = googleMap
 
+        databaseReference.addValueEventListener(object: ValueEventListener
+        {
+            override fun onDataChange(snapshot: DataSnapshot)
+            {
+                for (postSnapshot in snapshot.children) {
+                    // TODO: handle the post
+                    val etPlaceId = postSnapshot.child("id").value.toString()
+                    val etPlaceName = postSnapshot.child("name").value.toString()
+                    val etPlaceLat : Double = postSnapshot.child("LatLng/latitude").value as Double
+                    val etPlaceLng : Double = postSnapshot.child("LatLng/longitude").value as Double//.split(',')// (55.755826,37.6173)
+                    val etPlaceAddress = postSnapshot.child("address").value.toString()
+                    val moscowlog = LatLng(55.755826, 37.6173)
+                    val etPlaceLatLng = LatLng(etPlaceLat, etPlaceLng)
+                    Log.i("db reference info", "info ps - $postSnapshot,  ")
+                    Log.i("get from bd", "Place: ${etPlaceName}, || $etPlaceLat - $etPlaceLng ,== $moscowlog  == $etPlaceLatLng||")
+//                    if (moscowlog == etPlaceLatLng)
+//                        Log.i("log complete", "all good")
+                    mMap = googleMap
+                    mMap.addMarker(
+                        MarkerOptions()
+                        .position(etPlaceLatLng)
+                        .title(etPlaceName))
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("Error access to db", "All bad")
+            }
+        })
         // Add a marker in Sydney and move the camera
         val moscow = LatLng(55.7522, 37.6156)
-        mMap.addMarker(
-            MarkerOptions()
-                .position(moscow)
-                .title("Marker in Moscow"))
+//        mMap.addMarker(
+//            MarkerOptions()
+//                .position(moscow)
+//                .title("Marker in Moscow"))
+        mMap.maxZoomLevel
         mMap.moveCamera(CameraUpdateFactory.newLatLng(moscow))
     }
 }
