3f226944de3f9b5986d31f7fe64c8c9182c37f1c
Полина <gossipgirl2709@mail.ru>
2021-05-16 22:37:09 +0300

    Заглушка чата

---
+++ app/src/main/java/com/example/newmeetapp/ui/chat/ChatActivity.kt
@@ -0,0 +1,20 @@
+package com.example.newmeetapp.ui.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.content.Context
+import android.util.Log
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.newmeetapp.R
+import kotlinx.android.synthetic.main.activity_chat.*
+import java.util.*
+
+
+private const val TAG = "ChatActivity"
+
+class ChatActivity: AppCompatActivity() {
+
+}
---
+++ app/src/main/java/com/example/newmeetapp/ui/chat/MessageAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.newmeetapp.ui.chat
+
+import android.app.Notification
+import android.content.Context
+import java.text.SimpleDateFormat
+import java.util.*
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.newmeetapp.R
+import kotlinx.android.synthetic.main.my_message.view.*
+import kotlinx.android.synthetic.main.other_message.view.*
+
+private const val VIEW_TYPE_MY_MESSAGE = 1
+private const val VIEW_TYPE_OTHER_MESSAGE = 2
+
+class MessageAdapter (val context: Context) {
+}
